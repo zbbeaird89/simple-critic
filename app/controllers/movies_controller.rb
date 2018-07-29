2552ba83a0954b1cf164d541bf2cd66159b07c79
Zach Beaird <zbbeaird1989@gmail.com>
2018-07-29 06:41:52 -0500

    Users can create and view movies

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,31 @@
+class MoviesController < ApplicationController
+
+  def index
+    @movies = Movie.all
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to @movie
+    else
+      render "new"
+    end
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  private
+
+    def movie_params
+      params.require(:movie).permit(:title, :description)
+    end
+
+
+end
---
+++ spec/factories/movies.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :movie do
+    title "MyString"
+    description "MyText"
+  end
+end
