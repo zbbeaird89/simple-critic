944653251dff1e8c312bf8b7d6d4991fc8fdcda1
Zach Beaird <zbbeaird1989@gmail.com>
2018-09-18 20:21:12 -0500

    Adds about page and link

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "static_pages#home"
+  get "/about", to: "static_pages#about"
   resources :movies
   resources :users, :only => [:show]
 end
