17c193e79fcc1f5299d926a64c9dbd8cafa5f810
Zach Beaird <zbbeaird1989@gmail.com>
2018-10-29 12:21:42 -0500

    finishes user info section of user show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 
-  def gravatar_for(email, size)
+  def gravatar_for(email, size=200)
     gravatar = Digest::MD5::hexdigest(email).downcase
     url = "http://gravatar.com/avatar/#{gravatar}.png?s=#{size}"
   end
