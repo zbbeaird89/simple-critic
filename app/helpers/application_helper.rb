5cf92221d636a91bc4a8bc0ea032b8e2ee0903bc
Zach Beaird <zbbeaird1989@gmail.com>
2018-10-28 22:00:33 -0500

    adds gravatar_for to application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def gravatar_for(email, size)
+    gravatar = Digest::MD5::hexdigest(email).downcase
+    url = "http://gravatar.com/avatar/#{gravatar}.png?s=#{size}"
+  end
+
 end
