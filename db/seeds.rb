f54fedce0c6a878304aa0b65eae96f68587f910d
Zach Beaird <zbbeaird1989@gmail.com>
2018-11-26 20:50:18 -0600

    seeds database with updated associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,17 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
 12.times do |n|
+  name = Faker::Name.first_name
+  User.create(:username => name,
+              :email => "#{name}@name.com",
+              :password => "password",
+              :password_confirmation => "password")
+end
+
+12.times do |n|
   title = Faker::Book.title
   description = Faker::Lorem.sentence(10)
-  Movie.create(:title => title, :description => description)
+  Movie.create(:title => title,
+               :description => description,
+               :user_id => rand(1..12))
 end
