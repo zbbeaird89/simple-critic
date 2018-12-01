7065263aa22fd8b03e85c569caa70bc559c68a4a
Zach Beaird <zbbeaird1989@gmail.com>
2018-12-01 07:54:00 -0600

    creates genre model and sets up association with movies

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,5 @@
 class Movie < ApplicationRecord
   belongs_to :user
+  belongs_to :genre
   has_many :reviews
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181120034748) do
+ActiveRecord::Schema.define(version: 20181201134928) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -21,6 +27,7 @@ ActiveRecord::Schema.define(version: 20181120034748) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.integer "genre_id"
   end
 
   create_table "reviews", force: :cascade do |t|
