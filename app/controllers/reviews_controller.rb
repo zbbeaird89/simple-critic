b7379b57a65a2fe90f092279e8936d15bf629711
Zach Beaird <zbbeaird1989@gmail.com>
2018-12-12 05:49:07 -0600

    adds strong params for star ratings

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -24,6 +24,8 @@ class ReviewsController < ApplicationController
   private
 
     def review_params
-      params.require(:review).permit(:user_id, :movie_id, :opinion, :rating)
+      params.require(:review).permit(:user_id, :movie_id, :plot_rating, :acting_rating,
+                                     :screenplay_rating, :cinematography_rating, :musical_score_rating,
+                                     :editing_rating, :directing_rating, :overall_rating, :opinion)
     end
 end
