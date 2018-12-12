437be31d39b2b0ee24387a94866f4b871d02b83a
Zach Beaird <zbbeaird1989@gmail.com>
2018-12-12 12:31:14 -0600

    validates review columns excluding :overall_rating

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,12 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :movie
+
+  columns = [:plot_rating, :acting_rating, :screenplay_rating, :cinematography_rating,
+             :musical_score_rating, :editing_rating, :directing_rating]
+
+  columns.each do |column|
+    validates column, presence: true
+  end
+
 end
