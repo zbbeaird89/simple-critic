aa83a683fc6d1e8b637c34068a3ce278eb1e55f5
Zach Beaird <zbbeaird1989@gmail.com>
2020-12-09 05:31:06 -0600

    Tests and passes sign up request spec

--- spec/requests/signup_spec.rb
+++ spec/requests/signup_spec.rb
@@ -1,15 +1,17 @@
 require "rails_helper"
 
 RSpec.describe "user signup process", :type => :request do
+  let(:user) { build(:user) }
+
   context "successful signup" do
     it "gets signup page" do
       get new_user_registration_path
       expect(response.status).to eq 200
     end
 
-    it "redirects to root_path" do
+    it "redirects to movies_url" do
       post user_registration_path, params: { user: FactoryBot.attributes_for(:user) }
-      expect(response).to redirect_to root_url
+      expect(response).to redirect_to movies_url
     end
   end
 end
