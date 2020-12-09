bb216f1d343a7b14b30cc1484b4ed0da868ba6a3
Zach Beaird <zbbeaird1989@gmail.com>
2020-12-09 04:49:58 -0600

    Tests and passes a successful user login simulation

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,15 +1,15 @@
 require "rails_helper"
-
+#TODO test a successful login
 feature "Log In" do
-  before(:each) do
-    @user = create(:user)
-  end
+  let(:user) { create(:user) }
 
   scenario "Successful login" do
-    visit new_user_session_path
-    fill_in "user_username", :with => @user.username
-    fill_in "user_password", :with => @user.password
+    user
+    visit root_path
+    find(".dropdown-item", text: "Log In").click
+    fill_in "user[email]", :with => user.email
+    fill_in "user[password]", :with => user.password
     click_button "Log in"
-    expect(page).to have_content @user.username
+    expect(page).to have_selector('.review-btn', text: 'Add Movie')
   end
 end
