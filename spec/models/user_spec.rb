c5d11426b8b178c5715ae4e206715d7322c4a19b
Zach Beaird <zbbeaird1989@gmail.com>
2020-12-11 04:51:10 -0600

    Tests and passes association specs for User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,4 +10,21 @@ RSpec.describe User, type: :model do
     end
   end
 
+  context "Associations" do
+    it "has many movies" do
+      association = described_class.reflect_on_association(:movies)
+      expect(association.macro).to eq :has_many
+    end
+
+    it "has many reviews" do
+      association = described_class.reflect_on_association(:reviews)
+      expect(association.macro).to eq :has_many
+    end
+
+    it "has dependent: :destroy option" do
+      association = described_class.reflect_on_association(:reviews)
+      expect(association.options[:dependent]).to eq :destroy
+    end
+  end
+
 end
