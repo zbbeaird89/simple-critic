50adb23029fde1a911ed0eee65d6827112b8c334
Zach Beaird <zbbeaird1989@gmail.com>
2020-12-15 04:47:31 -0600

    Tests and passes associations for genre spec

--- spec/factories/genres.rb
+++ spec/factories/genres.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :genre do
+    association :movies, factory: :movies
     name { "MyString" }
   end
 end
--- spec/models/genre_spec.rb
+++ spec/models/genre_spec.rb
@@ -1,1 +1,12 @@
 require 'rails_helper'
+
+RSpec.describe Genre, :type => :model do
+  let(:genre) { build(:genre) }
+
+  context "Associations"do
+    it "has many movies" do
+      association = described_class.reflect_on_association(:movies)
+      expect(association.macro).to eq :has_many
+    end
+  end
+end
