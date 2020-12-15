4008d771314f61e170c42c8638cb46617c257cd5
Zach Beaird <zbbeaird1989@gmail.com>
2020-12-15 04:59:04 -0600

    Removes review_id association that shouldn't be there

--- spec/factories/movies.rb
+++ spec/factories/movies.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :movie do
+    association :user, factory: :user
+    association :genre, factory: :genre
     title { "MyString" }
     description { "MyText" }
   end
