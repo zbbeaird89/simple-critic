507526875b00ff44ed8bd4389adf4d0ecc6a99ad
Zach Beaird <zbbeaird1989@gmail.com>
2020-12-15 04:46:20 -0600

    Tests and passes validations and associations for review spec

--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -1,6 +1,16 @@
 FactoryBot.define do
   factory :review do
-    user { nil }
-    movie { nil }
+    association :user, factory: :user
+    association :movie, factory: :movie
+    opinion { Faker::Lorem.paragraph }
+    plot_rating { (1..5).to_a.sample }
+    acting_rating { (1..5).to_a.sample }
+    screenplay_rating { (1..5).to_a.sample }
+    cinematography_rating { (1..5).to_a.sample }
+    musical_score_rating { (1..5).to_a.sample }
+    editing_rating { (1..5).to_a.sample }
+    directing_rating { (1..5).to_a.sample }
+    average_rating { [plot_rating, acting_rating, screenplay_rating, cinematography_rating,
+                      musical_score_rating, editing_rating, directing_rating].sum / 7  } # 7 is number of fields
   end
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,1 +1,23 @@
 require 'rails_helper'
+
+RSpec.describe Review, :type => :model do
+  let(:review) { build(:review) }
+
+  context "Validations" do
+    it "is a valid review" do
+      expect(review).to be_valid
+    end
+  end
+
+  context "Associations" do
+    it "belongs to a user" do
+      association = described_class.reflect_on_association(:user)
+      expect(association.macro).to eq :belongs_to
+    end
+
+    it "belongs to a movie" do
+      association = described_class.reflect_on_association(:movie)
+      expect(association.macro).to eq :belongs_to
+    end
+  end
+end
