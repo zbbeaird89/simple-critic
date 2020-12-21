22522eb2afd87306664a7dc5c67837f69feff2a3
Zach Beaird <zbbeaird1989@gmail.com>
2020-12-21 05:18:13 -0600

    Tests successful and unsuccessful login requests

--- spec/requests/login_spec.rb
+++ spec/requests/login_spec.rb
@@ -1,9 +1,7 @@
 require "rails_helper"
 
 RSpec.describe "login process", :type => :request do
-  before(:each) do
-    @user = FactoryBot.create(:user)
-  end
+  let(:user) { create(:user) }
 
   it "gets login page" do
     get new_user_session_path
@@ -12,9 +10,23 @@ RSpec.describe "login process", :type => :request do
 
   context "successful login" do
     it "redirects to index" do
-      post user_session_path, params: { user: { email: @user.email,
-                                                password: @user.password } }
+      post user_session_path, params: { user: { email: user.email,
+                                                password: user.password } }
       expect(response).to redirect_to movies_url
+    end
+  end
+
+  context "unsuccessful login" do
+    it "renders login page when password is incorrect" do
+      post user_session_path, params: { user: { email: user.email,
+                                                password: "wrong password" } }
+      expect(response.body).to include("Log in")
+    end
+
+    it "renders login page when email is incorrect" do
+      post user_session_path, params: { user: { email: "stlprincess@gmail.com",
+                                                password: user.password } }
+      expect(response.body).to include("Log in")
     end
   end
 end
