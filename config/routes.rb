63fa34cb0ac6e7bda68f9fd3ca99339238e8bf97
Zach Beaird <zbbeaird1989@gmail.com>
2020-12-22 05:23:14 -0600

    Adds browser display of routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/rails/info/routes' => 'routes'
   devise_for :users, :controllers => { :registrations => "registrations" }
   root "static_pages#home"
   get "/about", to: "static_pages#about"
