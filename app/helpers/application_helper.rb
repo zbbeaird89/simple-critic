10f400a8c1be9cea20c4dbdb2d4030101ddb5999
Zach Beaird <zbbeaird1989@gmail.com>
2020-12-23 11:44:10 -0600

    Renders star rating for review card

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,9 @@
 require "movies_helper.rb"
+require "reviews_helper.rb"
 
 module ApplicationHelper
   include MoviesHelper
+  include ReviewsHelper
 
   def gravatar_for(email, size=200)
     gravatar = Digest::MD5::hexdigest(email).downcase
