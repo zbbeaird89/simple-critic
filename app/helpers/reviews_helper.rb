e80fb552775847f79b3eda0dc45eeaba41fb7cf3
Zach Beaird <zbbeaird1989@gmail.com>
2020-12-24 04:30:37 -0600

    Changes review card star ratings to show 5 stars to represent both on and off stars

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,11 @@
 module ReviewsHelper
+
+  def review_card_star_rating_partial_path(i, review)
+    if i < review.average_rating
+      "shared/cards/card/star-on.html.erb"
+    else
+      "shared/cards/card/star-off.html.erb"
+    end
+  end
+
 end
